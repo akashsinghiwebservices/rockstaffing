fefd9ace71b44a3d77524f8efece45c13ba94435
akashsinghiwebservices <76728846+akashsinghiwebservices@users.noreply.github.com>
2020-12-29 19:46:48 +0530

    Update function.js

--- common_function/function.js
+++ common_function/function.js
@@ -2,16 +2,25 @@ const config = require('../config/config')();
 const randomize = require('randomatic');
 const sgMail = require('@sendgrid/mail')
 var FCM = require('fcm-node');
+var admin = require("firebase-admin");
+// var serviceAccount = require("../readystaffing-firebase-adminsdk-9ex9v-0ef25da506.json");
+var serviceAccount = require("../readystaffing-907fb-firebase-adminsdk-8dvs1-f3bf10498f.json")
 
-// var serverKey = 'AAAAP0Zh1yk:APA91bH6A1r0K62VPpxX8vPFEwMDJwZFZ8LDolwxlp7HnwPnFuFq2nG0cE8-mRQNHaBqWnm0oYUp-wllyjRLQ3qPZfgsNND59HjP9E146r2Ulqry3KaJ5ySyxDuUetEYfSsIOfwkcKPV';
-var serverKey = 'AAAAAdQ_VSI:APA91bGaxmg1lhklXkFCccT8Rjhuv-j6Nv95rR2ggesTf6VHTfFSJRAgWdWzr1BgXcCSmm2ir052-6HKAeY-GjDYMP8VjtBimVVcTI8qSTZXAfP7yQ9l8xyB5mjnm5jemidnJY_y40Wg';
-// var serverKey = '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';
+admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount),
+    databaseURL: "https://readystaffing.firebaseio.com"
+});
 
-// var serverKey = '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'
-//var sendgridKey = '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';// client
-var sendgridKey = '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'; //me
+// var serverKey = 'AAAAAdQ_VSI:APA91bGaxmg1lhklXkFCccT8Rjhuv-j6Nv95rR2ggesTf6VHTfFSJRAgWdWzr1BgXcCSmm2ir052-6HKAeY-GjDYMP8VjtBimVVcTI8qSTZXAfP7yQ9l8xyB5mjnm5jemidnJY_y40Wg';
+
+var serverKey = 'AAAAm5qMce0:APA91bFSZyB6p-zVSFwzSakCC8a24D6FmmsukIuzZEOPzH7fhGj5dn4fD4MRFSKUNYhVTC-ZfNmag1JzRCdw2GyqozTpeUEpza4xT9DFNVG8G0PvtdceR4owKDSqsiNLY0Wj7cTT7S9O';
+
+
+
+var sendgridKey = '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';
 
 var fcm = new FCM(serverKey);
+
 
 
 const all_function = {
@@ -50,36 +59,101 @@ const all_function = {
             text: text,
             html: html,
         };
+
+        console.log("messageObject ===>", msg);
         sgMail.send(msg).then((sent) => {
-            console.log('message sent')
+            console.log('message sent ===>')
         }).catch((err) => {
             console.log('message not sent', err)
         })
     },
 
 
+    // send email with attachement via sendGrid
+
+    'sendEmailWithAttachment': (to, subject, text, attachment) => {
+        sgMail.setApiKey(sendgridKey);
+
+        const msg = {
+            to: to,
+            from: 'no-reply@rockstaffing.com',
+            subject: subject,
+            text: text,
+            attachments: attachment
+        };
+
+        // console.log("messageObject ===>", msg);
+        sgMail.send(msg).then((sent) => {
+            console.log('attachement sent ===>')
+        }).catch((err) => {
+            console.log('attachment not sent', err)
+        })
+    },
+
     'send_push_notification': (fcm_token, title, body, data) => {
 
-        var message = {
-            //this may vary according to the message type (single recipient, multicast, topic, et cetera)
-            to: fcm_token,
-            collapse_key: 'green',
+        // var message = {
+        //     //this may vary according to the message type (single recipient, multicast, topic, et cetera)
+        //     to: fcm_token,
+        //     collapse_key: 'green',
 
-            notification: {
+        //     notification: {
+        //         title: title,
+        //         body: body
+        //     },
+        //     data: data
+        // }
+
+        // fcm.send(message, (err, response) => {
+
+        //     if (err) {
+        //         console.log("Something has gone wrong!", err)
+        //     } else {
+        //         console.log("Successfully sent with response: ", response)
+        //     }
+        // })
+
+
+        var message = {
+            "token": fcm_token,
+            "notification": {
                 title: title,
                 body: body
             },
-            data: data
-        }
-
-        fcm.send(message, (err, response) => {
-
-            if (err) {
-                console.log("Something has gone wrong!")
-            } else {
-                console.log("Successfully sent with response: ", response)
+            "data": data,
+            "android": {
+                "priority": "high",
+                "notification": {
+                    "click_action": "FLUTTER_NOTIFICATION_CLICK"
+                },
+            },
+            "apns": {
+                "headers": {
+                    "apns-priority": "5",
+                },
+                "payload": {
+                    "aps": {
+                        "category": "NEW_MESSAGE_CATEGORY"
+                    }
+                }
+            },
+            "webpush": {
+                "headers": {
+                    "Urgency": "high"
+                }
             }
-        })
+        };
+
+        // end a message to the device corresponding to the provided
+        // registration token.
+        admin.messaging().send(message)
+            .then((response) => {
+                // Response is a message ID string.
+                console.log('Successfully sent message:', response);
+            })
+            .catch((error) => {
+                console.log('Error sending message:', error);
+            });
 
     },
 
